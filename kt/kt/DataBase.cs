4be15c249b99942f8fc5d700908182ad3b3a524e
stucksippin <regulationqq@mail.ru>
2022-10-17 20:17:58 +0300

    kt 8

---
+++ kt/kt/DataBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPractise
+{
+    internal class DataBase<T>
+    {
+        List<T> objects = new List<T>();
+        internal void Add(T value)
+        {
+            objects.Add(value);
+        }
+        internal void Output()
+        {
+            foreach (var item in objects)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        internal void OutputType(string type)
+        {
+            foreach (var item in objects)
+            {
+                switch (type)
+                {
+                    case "System.Int32":
+                        try
+                        {
+                            int parsed = Convert.ToInt32(item);
+                            var t = parsed.GetType();
+                            string ts = t.ToString();
+                            if (ts == type)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ kt/kt/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace GenericsPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase<object> database = new DataBase<object>();
+            Console.WriteLine("Добро пожаловать в базу данных! \n 1. Добавить элемент \n 2. Вывести список на экран \n 3. Вывести определённые эмементы списка");
+            int read;
+            do
+            {
+                int.TryParse(Console.ReadLine(), out read);
+                switch (read)
+                {
+                    case 1:
+                        Console.Write("enter the element: ");
+                        database.Add(Console.ReadLine());
+                        Console.WriteLine("The element is recorded. You can select one of the menu items again:\n 1. Add an element \n 2. Display the list on the screen \n 3. Display certain elements of the list");
+                        break;
+                    case 2:
+                        Console.WriteLine("elements");
+                        database.Output();
+                        Console.WriteLine("1. Add an element \n 2. Display the list on the screen \n 3. Display certain elements of the list");
+                        break;
+                    case 3:
+                        Console.Write("Enter the required data type:");
+                        string datat = Console.ReadLine();
+                        switch (datat)
+                        {
+                            case "int":
+                                string i = "System.Int32";
+                                database.OutputType(i);
+                                break;
+                            default:
+                                Console.WriteLine("You entered the data type incorrectly");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Select one of the menu items");
+                        continue;
+                }
+            }
+            while (true);
+
+            Console.ReadLine();
+        }
+    }
+}
